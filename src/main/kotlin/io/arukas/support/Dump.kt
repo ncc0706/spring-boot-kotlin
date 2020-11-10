339bd1a0d93fa5505658d7f2b4823638e9dd9dcf
niuyuxian <ncc0706@gmail.com>
2020-11-10 20:47:33 +0800

    add comments

--- src/main/kotlin/io/arukas/support/Dump.kt
+++ src/main/kotlin/io/arukas/support/Dump.kt
@@ -8,24 +8,51 @@ import com.fasterxml.jackson.annotation.JsonInclude
  * @date 2020/11/08 22:12
  */
 data class Dump(
+
+        /**
+         * 状态码
+         */
         val status: Int,
+
+        /**
+         * 报文描述
+         */
         val message: String,
+
+        /**
+         * 当前时间戳
+         */
         val timestamp: Long,
+
+        /**
+         * 报文数据
+         */
         @JsonInclude(JsonInclude.Include.NON_EMPTY) val data: Any) {
 
-//    var status = 200
-//    var message = ""
-//    var timestamp = System.currentTimeMillis()
-//
-//    @JsonInclude(JsonInclude.Include.NON_EMPTY)
-//    var data = Any()
-
+    /**
+     * 伴生对象
+     */
     companion object {
 
+        /**
+         * 默认成功响应
+         */
         fun success() = Dump(200, "success", System.currentTimeMillis(), Any())
+
+        /**
+         * @param message 报文描述
+         */
         fun success(message: String) = Dump(200, message, System.currentTimeMillis(), Any())
+
+        /**
+         * @param message 报文描述
+         * @param data    报文数据
+         */
         fun success(message: String, data: Any) = Dump(200, message, System.currentTimeMillis(), data)
 
+        /**
+         * 默认失败响应
+         */
         fun error() = Dump(500, "server error", System.currentTimeMillis(), Any())
 
     }
